64e0583d45e09bc8fd3c62b0f681789ba07a2753
dev <dev@rehat.com>
2017-02-16 08:08:49 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var os = require('os');
+var ip = require('ip');
+var app = express();
+var greeting = process.env.GREETING || "World";
+
+function say_hello(){
+    return "Hello " + greeting + "<br> My hostname is " + os.hostname() + "<br> My IP address is " + ip.address();
+}
+
+app.get('/api/hello', function(req, resp) {
+    resp.set('Access-Control-Allow-Origin', '*');
+    resp.send(say_hello());
+});
+
+app.get('/', function(req, resp) {
+    resp.set('Access-Control-Allow-Origin', '*');
+    resp.send('endpoint: <br> <a href="/api/hello">/api/hello</a>');
+});
+
+app.get('/api/health', function(req, resp) {
+    resp.set('Access-Control-Allow-Origin', '*');
+    resp.send("I'm ok");
+});
+
+var server = app.listen(8080, '0.0.0.0', function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Hello service running at http://%s:%s", host, port)
+});
