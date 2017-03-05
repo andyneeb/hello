30962d4ead797d5c30a46c0e2efb9aa9f03517f3
Andreas Neeb <aneeb@redhat.com>
2017-03-05 22:59:59 +0100

    demo

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var ip = require('ip');
 var app = express();
 var greeting = process.env.GREETING;
 var who = process.env.WHO;
+var fs = require('fs');
 
 function say_hello(){
-    return greeting + " " + who + " from Container " + os.hostname() + " with IP " + ip.address();
+    return greeting + " " + who + " from Container " + os.hostname() + " with IP " + ip.address() + " in " + fs.data();
 }
+
+fs.readFile('/var/run/secrets/kubernetes.io/serviceaccount/namespace', 'utf8', function(err, data) {  
+    if (err) throw err;
+    console.log(data);
+});
 
 app.get('/api/hello', function(req, resp) {
     resp.set('Access-Control-Allow-Origin', '*');
