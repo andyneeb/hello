62f24d17a45261f575060d6920e1859d47dbf597
Andreas Neeb <aneeb@redhat.com>
2017-03-20 17:01:11 +0100

    automated push

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var fs = require("fs");
 var namespace = fs.readFileSync("/var/run/secrets/kubernetes.io/serviceaccount/namespace").toString();
 
 function say_hello(){
-    return greeting + " " + who + "! I am Pod: " + os.hostname() + " My IP: " + ip.address();
+    return greeting + " " + who + "! from Pod: " + os.hostname() + " with IP: " + ip.address();
 }
 
 function get_namespace(){
