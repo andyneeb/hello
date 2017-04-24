40aec981c058aa20190823423ce7a609d646b717
Andreas Neeb <aneeb@redhat.com>
2017-04-24 23:20:08 +0200

    automated push

--- curl.sh
+++ curl.sh
@@ -1,1 +1,1 @@
-clear; while true; do curl http://hello-dev.apps.192.168.122.100.xip.io/api/hello; echo -e; sleep 1;done
+clear; while true; do curl http://hello-dev.192.168.42.164.nip.io/api/hello; echo -e; sleep 1;done
