ccd8a686579a6f35b93e76963bf2eb490b37b3de
Andreas Neeb <aneeb@redhat.com>
2017-05-21 07:02:11 +0200

    automated push

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var who = process.env.WHO;
 var fs = require("fs");
 var namespace = fs.readFileSync("/var/run/secrets/kubernetes.io/serviceaccount/namespace").toString();
 
-function say_hello(){
+function Xsay_hello(){
     return greeting + " " + who + "! Hostname: " + os.hostname() + " IP: " + ip.address();
 }
 
