31394467c382572603ddd3b8356bcf31119be151
Andreas Neeb <aneeb@redhat.com>
2017-05-21 16:43:13 +0200

    automated push

--- demo/build.sh
+++ demo/build.sh
@@ -5,7 +5,7 @@ oc login -u dev -p redhat
 oc project dev
 
 # Build from Source and Deploy
-oc new-app nodejs:4~http://gogs.192.168.42.164.nip.io/dev/hello.git --name=hello
+oc new-app nodejs:4~http://gogs.192.168.42.253.nip.io/dev/hello.git --name=hello
 
 # Create route
 oc expose svc hello
