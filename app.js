a54e48fdd7c073d0dd547bf7f0d119c12e89b6b0
Andreas Neeb <aneeb@redhat.com>
2017-07-10 21:39:15 +0200

    # testing jbdv integration

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var fs = require("fs");
 var namespace = fs.readFileSync("/var/run/secrets/kubernetes.io/serviceaccount/namespace").toString();
 
 function say_hello(){
-    return greeting + " " + who + "! Hostname: " + os.hostname() + " IP: " + ip.address();
+    return greeting + " " + who + " ! Hostname: " + os.hostname() + " IP: " + ip.address();
 }
 
 function get_namespace(){
