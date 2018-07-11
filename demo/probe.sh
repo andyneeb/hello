da8f712c4063726edfabe8785b33ccbac9421906
Andreas Neeb <aneeb@redhat.com>
2018-07-11 22:30:46 +0200

    adding healthz endpoint

--- demo/probe.sh
+++ demo/probe.sh
@@ -1,1 +1,2 @@
-oc set probe dc/hello --readiness --get-url=http://:8080/api/hello
+oc set probe dc/hello --readiness --get-url=http://:8080/healthz
+oc set probe dc/hello --liveness --get-url=http://:8080/healthz
