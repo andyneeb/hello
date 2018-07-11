75dfd8386b2231d5175e784e70a9829520e28301
Andreas Neeb <aneeb@redhat.com>
2018-07-11 22:51:00 +0200

    cleaning some obsolete files

--- demo/curl.sh
+++ demo/curl.sh
@@ -1,1 +1,1 @@
-clear; while true; do curl http://hello-dev.192.168.42.253.nip.io/api/hello; echo -e;sleep 1;done
+clear; while true; do curl http://$(oc get route hello | grep hello | awk 'NR==1{print $2}')/api/hello; echo -e;sleep 1;done
