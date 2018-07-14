474bf2eedb74d77917f8830da045a3adae9c992e
Andreas Neeb <aneeb@redhat.com>
2018-07-14 16:15:45 +0200

    fixing curl script

--- demo/curl.sh
+++ demo/curl.sh
@@ -1,1 +1,1 @@
-clear; while true; do curl http://$(oc get route hello | grep hello | awk 'NR==1{print $2}')/api/hello; echo -e;sleep 1;done
+clear; while true; do curl http://$(oc get route hello | grep hello | awk 'NR==1{print $2}')/hello; echo -e;sleep 1;done
