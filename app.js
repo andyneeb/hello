2e6d8a83fca2ad6e804adc0334c7473efaa50b63
Andreas Neeb <aneeb@redhat.com>
2018-07-14 16:07:32 +0200

    fixing hello output formating

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var healthy=true;
 var app = express();
 
 app.get('/hello', function (req, res) {
-  res.send(greeting + ' ' + who + '\n Container ' + os.hostname() + '\n Address' + ip + '\n');
+  res.send(greeting + ' ' + who + ' --- Container ' + os.hostname() + ' --- Address ' + ip + '\n');
 });
 
 app.get('/file', function(req, res) {
